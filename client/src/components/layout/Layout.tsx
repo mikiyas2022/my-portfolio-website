59b5c2c754ce8cf17f50f89189180d5105eb07ab
mikiyasET <mikiyasleul1@gmail.com>
2024-12-24 00:40:50 +0300

    Fix Navbar module issue

--- client/src/components/layout/Layout.tsx
+++ client/src/components/layout/Layout.tsx
@@ -1,22 +1,22 @@
-import { Outlet, useNavigate } from 'react-router-dom';
+import React from 'react';
+import { Box } from '@mui/material';
+import { Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { AppBar, Toolbar, Typography, Button, Container, Box } from '@mui/material';
-import { RootState } from '../../redux/store';
 import Navbar from './Navbar';
+import { RootState } from '../../redux/store';
 
-const Layout = () => {
+const Layout: React.FC = () => {
+  const { token } = useSelector((state: RootState) => state.auth);
+
+  if (!token) {
+    return <Outlet />;
+  }
+
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
       <Navbar />
-      <Container component="main" sx={{ flexGrow: 1, py: 4, mt: 8 }}>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <Outlet />
-      </Container>
-      <Box component="footer" sx={{ py: 3, bgcolor: 'background.paper' }}>
-        <Container maxWidth="lg">
-          <Typography variant="body2" color="text.secondary" align="center">
-            © {new Date().getFullYear()} Portfolio Platform. All rights reserved.
-          </Typography>
-        </Container>
       </Box>
     </Box>
   );
